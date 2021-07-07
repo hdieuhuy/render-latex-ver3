f47c4386145cf3861c0235fff7eadeb3d7fe6cc4
Dieu Huy <hdieuhuy227@gmail.com>
2021-07-07 22:14:56 +0700

    update configmathjax

--- src/components/Test.js
+++ src/components/Test.js
@@ -3,14 +3,11 @@ import React, { useEffect } from 'react';
 // \\newcommand{\\heva}[1]{\\left\\{ \\begin{aligned}{#1}\\end{aligned}\\right.}
 // \\newcommand{\\hoac}[1]{ \\left[\\begin{aligned}#1\\end{aligned}\\right.}
 
-const latex = `$
-
-\\hoac{a+b=1 \\\\ c+d=2}
-
-$`;
+const latex = `Cho hàm số $f(x)=\\heva{2x-1 \\quad \\text{khi} \\quad x \\ge 1 \\\\ 3x^2-2 \\quad \\text{khi} \\quad x \\lt 1}.$ Giả sử $F$ là nguyên hàm của $f$ trên $\\mathbb{R}$ thỏa mãn $F(0)=2$. Giá trị của $F(-1)+2F(2)$ bằng`;
 
 // const text = `$a+b = 1$`;
-// const test = `$\\newcommand{\\heva}[1]{\\left\\{ \\begin{aligned}#1\\end{aligned}\\right.}$`;
+// const test = `$
+// Cho hàm số $f(x)=\\heva{2x-1 \\quad \\text{khi} \\quad x \\ge 1 \\\\ 3x^2-2 \\quad \\text{khi} \\quad x \\lt 1}.$ Giả sử $F$ là nguyên hàm của $f$ trên $\\mathbb{R}$ thỏa mãn $F(0)=2$. Giá trị của $F(-1)+2F(2)$ bằng$`;
 
 const Test = () => {
   // reload Mathjax
@@ -35,9 +32,6 @@ const Test = () => {
   return (
     <div
       style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
         margin: '120px',
       }}
     >
