2bbece2a9687e37040274e051f789c1a74f21670
dieuhuy227 <hdieuhuy227@gmail.com>
2022-05-18 09:57:39 +0700

    fix basrUrrl

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 // var axios = require('axios');
 // var qs = require('qs');
-const baseUrl = 'https://node-render-latex.herokuapp.com';
+const baseUrl = 'https://node-render-exam.herokuapp.com';
 
 const getExam = async (examCode) => {
   const res = await axios.get(`${baseUrl}/api/exam/${examCode}`);
